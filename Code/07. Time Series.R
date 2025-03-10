b12844ab0c42c20de5c8b37ff1e780e8aac0a9f3
bnglasner <54865110+bnglasner@users.noreply.github.com>
2025-03-10 13:21:44 -0400

    Update 07. Time Series.R

--- Code/07. Time Series.R
+++ Code/07. Time Series.R
@@ -19,6 +19,8 @@ library(ggplot2)
 # Define user-specific project directories
 project_directories <- list(
   "name" = "PATH TO GITHUB REPO",
+  "Benjamin Glasner" = "C:/Users/Benjamin Glasner/EIG Dropbox/Benjamin Glasner/GitHub/oz-housing-supply",
+  "bngla" = "C:/Users/bngla/EIG Dropbox/Benjamin Glasner/GitHub/oz-housing-supply"
 )
 
 # Setting project path based on current user
